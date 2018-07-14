557104d10a651ebe4a03a2f0348bb99e8b57cc6d
Svetoslav Blyahov <svetoslav@tradeo.com>
2018-07-14 14:14:36 +0300

    Start handling messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,50 @@
 class MessagesController < ActionController::API
+  ERROR_RESPONSE_CODE = 412
+  SUCCESS_RESPONSE_CODE = 200
+  VALID_TYPES = %w(send_emotion send_text).freeze
+
   def create
-    render json: { message: 'everything is fine. calm down' }
+    render response_content
+  end
+
+  private
+
+  def response_content
+    return error_response if invalid_params? || unprocessed_message?
+    success_response
+  end
+
+  def invalid_params?
+    invalid_message_type? || invalid_payload?
+  end
+
+  def invalid_message_type?
+    VALID_TYPES.exclude? params[:type]
+  end
+
+  def invalid_payload?
+    request.GET[:payload].present? ||
+      params[:payload].blank?
+  end
+
+  def unprocessed_message?
+    message = Message.new(
+      message_type: extracted_type,
+      payload: params[:payload]
+    )
+
+    !message.save
+  end
+
+  def extracted_type
+    params[:type].split('_').last
+  end
+
+  def error_response
+    { status: ERROR_RESPONSE_CODE }
+  end
+
+  def success_response
+    { status: SUCCESS_RESPONSE_CODE }
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,39 @@
 class Message < ApplicationRecord
-  enum message_type: %i(emotion text)
+  SHORT_TYPE = 'emotion'.freeze
+  REGULAR_TYPE = 'text'.freeze
+  PAYLOAD_RANGES = {
+    SHORT_TYPE => [2, 10],
+    REGULAR_TYPE => [1, 160]
+  }.freeze
+  MESSAGE_TYPES = PAYLOAD_RANGES.keys.freeze
+
+  enum message_type: MESSAGE_TYPES
+  validates :message_type, presence: true, inclusion: { in: MESSAGE_TYPES }
+  validates :payload, presence: true
+  validate :payload_length
+  validate :payload_no_digits, if: -> { message_type == SHORT_TYPE }
+
+  private
+
+  def payload_length
+    errors.add(:payload, "Size not in #{payload_range}") if payload_out_of_range?
+  end
+
+  def payload_range
+    PAYLOAD_RANGES[message_type]
+  end
+
+  def payload_out_of_range?
+    return false unless payload
+    !payload.size.between? *payload_range
+  end
+
+  def payload_no_digits
+    errors.add(:payload, "#{SHORT_TYPE} type can't contain any digits") if payload_with_digits?
+  end
+
+  def payload_with_digits?
+    return false unless payload
+    !!(payload =~ /\d/)
+  end
 end
